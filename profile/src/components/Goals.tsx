943c9fdc57e76c9193deab6819e258052e49a223
Travis <travisjb0@gmail.com>
2023-08-26 21:30:40 -0700

    Added Goals Section

---
+++ profile/src/components/Goals.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Goals = () => {
+  return (
+    <div>
+      <h1>Goals</h1>
+      <h2>September 2023 - Finish Eccomerce learning and site. </h2>
+    </div>
+  )
+}
+
+export default Goals
